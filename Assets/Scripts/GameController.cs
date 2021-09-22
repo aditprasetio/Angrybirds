439089c6969fc5a0dee2cd951953ba0d02273272
aditprasetio <aditprasetio16@gmail.com>
2021-09-22 20:23:05 +0700

    chapter5

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+        }
+
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void ChangeBird()
+    {
+        Birds.RemoveAt(0);
+
+        if (Birds.Count > 0)
+            SlingShooter.InitiateBird(Birds[0]);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
